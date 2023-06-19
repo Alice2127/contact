6da2f5eb689aa2b4a4400fd17158fd2c5377f10a
Alice2127 <aiokada2128@gmail.com>
2023-06-19 17:54:53 +0900

    call my_components.ex with arguments but it doesn't works

--- lib/contact_web/components/my_components.ex
+++ lib/contact_web/components/my_components.ex
@@ -62,4 +62,28 @@ defmodule ContactWeb.Mycomponents do
     """
   end
 
+  #assignsの中身を必要とするコンポーネント（こちらが動かない）
+  def input(%{type: "fuga"} = assigns) do
+    IO.inspect("独自のコンポーネント2")
+    ~H"""
+    <div phx-feedback-for={@name}>
+      <.label for={@id}><%= @label %></.label>
+      <textarea
+        id={@id}
+        name={@name}
+        rows={@rows}
+        class={[
+          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
+          "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
+          @errors == [] && "border-zinc-300 focus:border-zinc-400",
+          @errors != [] && "border-rose-400 focus:border-rose-400"
+        ]}
+        {@rest}
+      ><%= Phoenix.HTML.Form.normalize_value("textarea", @value) %></textarea>
+      <.error :for={msg <- @errors}><%= msg %></.error>
+    </div>
+    """
+  end
+
+
 end
